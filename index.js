d7dee7770e06a8c0cc2fdb0bc7ae54a70642b25c
Steven <fernandezklein123@gmail.com>
2026-01-18 13:16:23 +0800

    Day1

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-let age = 10;
+let products = [];
 
-function showAge(age){
-    return age;
-}
+products.push("Shampoo");
+products.push("Sabon");
+products.push("Toothpaste");
 
-console.log(showAge(age));
+
+for(let i=0; i < products.length; i++)
+    {
+        console.log(products[i]);
+    }
+
+
+console.log(`First product in array : ${products[0]}`);
+console.log(`Last product in array : ${products[products.length-1]}`);
+
+
+for(let i=0; i < products.length; i++)
+    {
+        if(products[i].toLocaleLowerCase() === "sabon")
+            {
+                products[i] = "Liquid Soap";
+            }
+    }
+
+
+    console.log(products);
+
+    products.splice(i, 1);
+
+    console.log(products);
